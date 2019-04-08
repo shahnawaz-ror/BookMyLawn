48a304d0290998de840379fd21da9fe1b5ef3d35
Mohammad Shahnawaz <shahnawazkits@gmail.com>
2019-04-08 10:57:20 +0530

    (WIP) Added the new design.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_093508) do
+ActiveRecord::Schema.define(version: 2019_04_07_152001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: :cascade do |t|
+    t.string "image"
+    t.boolean "active"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subdomains", force: :cascade do |t|
     t.string "name"
